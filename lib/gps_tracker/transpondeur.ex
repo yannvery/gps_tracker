f00329792a02cbef595acb5e0eb92a11ec3ac1ac
Yann VERY <yann.very@gmail.com>
2019-09-08 15:28:49 +0200

    Transpondeur emits a position after moving for 5 meters

--- lib/gps_tracker/transpondeur.ex
+++ lib/gps_tracker/transpondeur.ex
@@ -24,23 +24,54 @@ defmodule GpsTracker.Transpondeur do
 
   @impl true
   def init(endpoint) do
-    {:ok, endpoint}
+    {:ok, %{endpoint: endpoint, current_position: nil}}
   end
 
   @impl true
-  def handle_cast({:emit, position}, endpoint) do
+  def handle_cast(
+        {:emit, position},
+        state = %{endpoint: endpoint, current_position: nil}
+      ) do
+    {:ok, json} = Poison.encode(position)
+    post_to(endpoint, json)
+
+    {:noreply, %{state | current_position: position}}
+  end
+
+  def handle_cast(
+        {:emit, position},
+        state = %{endpoint: endpoint, current_position: current_position}
+      ) do
+    with true <- position_issued_after?(position, current_position),
+         {:ok, distance} <- GpsTracker.Distance.compute(position, current_position),
+         {:ok, distance_in_meters} <- GpsTracker.Distance.to_meters(distance),
+         true <- distance_in_meters > 5.0 do
+      post_to(endpoint, position)
+      {:noreply, %{state | current_position: position}}
+    else
+      _ ->
+        {:noreply, state}
+    end
+  end
+
+  @impl true
+  def handle_call(:pid, _from, state) do
+    {:reply, self(), state}
+  end
+
+  def position_issued_after?(position, current_position) do
+    with {position_time, ""} <- Float.parse(Map.get(position, :time, "0")),
+         {current_position_time, ""} <- Float.parse(Map.get(current_position, :time, "0")) do
+      position_time > current_position_time
+    end
+  end
+
+  defp post_to(endpoint, position) do
     {:ok, json} = Poison.encode(position)
 
     HTTPotion.post(endpoint,
       body: json,
       headers: ["User-Agent": "My App", "Content-Type": "application/json"]
     )
-
-    {:noreply, endpoint}
-  end
-
-  @impl true
-  def handle_call(:pid, _from, state) do
-    {:reply, self(), state}
   end
 end
