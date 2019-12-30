20dacc6e09c5d25fc3951294c394e9c5bfeaec9b
Yann VERY <yann.very@gmail.com>
2019-12-30 10:18:14 +0100

    Bump to nerves 1.6

--- mix.exs
+++ mix.exs
@@ -1,18 +1,22 @@
 defmodule GpsTracker.MixProject do
   use Mix.Project
 
+  @app :gps_tracker
+
   @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64]
 
   def project do
     [
-      app: :gps_tracker,
+      app: @app,
       version: "0.1.0",
-      elixir: "~> 1.8",
-      archives: [nerves_bootstrap: "~> 1.5"],
+      elixir: "~> 1.9",
+      archives: [nerves_bootstrap: "~> 1.6"],
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
       aliases: [loadconfig: [&bootstrap/1]],
-      deps: deps()
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host]
     ]
   end
 
@@ -31,14 +35,25 @@ defmodule GpsTracker.MixProject do
     ]
   end
 
+  def release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.4", runtime: false},
-      {:shoehorn, "~> 0.4"},
+      {:nerves, "~> 1.5.1", runtime: false},
+      {:shoehorn, "~> 0.6"},
       {:ring_logger, "~> 0.6"},
       {:toolshed, "~> 0.2"},
+      {:distillery, "~> 2.1"},
 
       # UART
       {:circuits_uart, "~> 1.3"},
@@ -55,13 +70,15 @@ defmodule GpsTracker.MixProject do
       {:httpotion, "~> 3.1.0"},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.6", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.6", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.6", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.6", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.6", runtime: false, targets: :rpi3a},
-      {:nerves_system_bbb, "~> 2.0", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.6", runtime: false, targets: :x86_64}
+      {:nerves_system_rpi, "~> 1.8", runtime: false, targets: :rpi},
+      # {:nerves_system_rpi0, "~> 1.8", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi0, "~> 1.8.2", nerves: [compile: true], runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.8", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.8", nerves: [compile: true], runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.8", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.8", runtime: false, targets: :rpi4},
+      {:nerves_system_bbb, "~> 2.3", runtime: false, targets: :bbb},
+      {:nerves_system_x86_64, "~> 1.8", runtime: false, targets: :x86_64}
     ]
   end
 end
